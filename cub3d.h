f64c4422fface80af545f721f02c6ce1dcbf7c2e
Wout Martens <WoutMartens94@gmail.com>
2021-02-02 15:17:39 +0100

    h

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: y4k_wm <y4k_wm@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/18 18:20:41 by y4k_wm        #+#    #+#                 */
-/*   Updated: 2020/11/18 18:23:45 by y4k_wm        ########   odam.nl         */
+/*   Updated: 2021/02/02 15:17:32 by wmartens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <fcntl.h>
 # include "c_struct.h"
 # include <stdio.h>
+# include <math.h>
 
 void	init_struct(t_cub3 *c_struct);
 void	parser(char *map, t_cub3 *c_struct);
